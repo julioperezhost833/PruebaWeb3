d6c8387652c40d4a2df308df2df740113a11e32e
julioperezhost833 <julio.perez@host833.com>
2025-01-14 13:09:32 -0600

    primer commit

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// Este archivo contiene el código JavaScript para la página web. Puede incluir interactividad y manipulación del DOM.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('La página ha sido cargada y está lista para la interacción.');
+
+    // Aquí puedes agregar más código para la interactividad
+});
